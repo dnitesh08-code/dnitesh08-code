f16b20b10fabccf035c5c2fb4be5f592d2810bb1
dnitesh08-code <d.nitesh08@gmail.com>
2026-09-21 23:44:17 -0400

    Publish updated web developer portfolio

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('details').forEach((detail) => {
+  detail.addEventListener('toggle', () => {
+    if (detail.open) {
+      document.querySelectorAll('details').forEach((other) => {
+        if (other !== detail) other.open = false;
+      });
+    }
+  });
+});
+
